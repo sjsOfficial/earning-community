fb8f4c03d580610fbb63ee8af39ba49ae3667310
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-03-06 14:23:09 +0600

    okay

--- earningcommunity-project/components/Authentication/JoinUs.tsx
+++ earningcommunity-project/components/Authentication/JoinUs.tsx
@@ -4,26 +4,37 @@ import React from "react";
 export default function JoinUs() {
   return (
     <div className="container mx-auto mt-[37px]">
-      <div className="bg-[#85929E] rounded-[10px] p-[20px] flex justify-between items-center">
-        <div className="max-w-[950px]">
-          <h1 className="text-[#FFFFFF] font-medium text-[24px]">
-            Join with us
-          </h1>
-          <p className="text-[#FFFFFF] font-medium text-[20px] mt-[12px] mb-[12px]">
-            With our supportive community and innovative monetization features,
-            you can turn your passion into profit. Participate in tournaments,
-            collaborate with fellow creators, and engage with your audience to
-            maximize your earnings potential.
-          </p>
-          <div className="py-4 bg-[#2E4053] rounded-[10px] max-w-[350px]">
-            <p className="text-[#FFFFFF] font-medium text-[20px] text-center">
-              Register Now
+      <div className="bg-[#85929E] mx-2 rounded-[10px] p-[20px] ">
+        <div className="gap-2 flex justify-between items-center">
+          <div className="w-[286px] md:w-[950px]">
+            <h1 className="text-[#FFFFFF] font-medium text-[16px] md:text-[24px]">
+              Join with us
+            </h1>
+            <p className="text-[#FFFFFF] font-normal teext-[14px] md:text-[20px] mt-[12px] mb-[12px]">
+              With our supportive community and innovative monetization
+              features, you can turn your passion into profit. Participate in
+              tournaments, collaborate with fellow creators, and engage with
+              your audience to maximize your earnings potential.
             </p>
+            <div className="hidden md:block py-2 md:py-4 bg-[#2E4053] hover:bg-[#1b2a3e] rounded-[10px] max-w-[350px] cursor-pointer transition-colors duration-500 ease-in-out">
+              <p className="text-[#FFFFFF] font-medium teext-[14px] md:text-[20px] text-center">
+                Register Now
+              </p>
+            </div>
+          </div>
+          <div className="w-[86px] md:w-auto">
+            <Image
+              className=""
+              src={require("../../public/earnNow.svg")}
+              alt="add"
+            ></Image>
           </div>
         </div>
-        <div>
-          <Image src={require("../../public/earnNow.svg")} alt="add"></Image>
-        </div>
+        <div className=" md:hidden py-2 md:py-4 bg-[#2E4053] hover:bg-[#1b2a3e] rounded-[10px] cursor-pointer transition-colors duration-500 ease-in-out">
+              <p className="text-[#FFFFFF] font-medium teext-[14px] md:text-[20px] text-center">
+                Register Now
+              </p>
+            </div>
       </div>
     </div>
   );
