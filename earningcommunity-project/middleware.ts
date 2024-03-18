ff8de5e1571c5bbd505257ed88b9b3318ecd9e29
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2024-03-18 15:33:52 +0600

    dsf

--- earningcommunity-project/middleware.ts
+++ earningcommunity-project/middleware.ts
@@ -1,23 +1,42 @@
 import { NextRequest, NextResponse, userAgent } from 'next/server'
+import jwt from "jsonwebtoken"
+
+interface PlayLoad {
+  username: string,
+  userId: string
+}
 
 export function middleware(request: NextRequest) {
-  
-  const url = request.nextUrl
-  const agent = userAgent(request)
-  const browser = agent.browser;
-  const device = agent.device
-  const os = agent.os
- 
-  url.searchParams.set('browserName', browser.name ? browser.name : "t")
-  url.searchParams.set('browserVersion', browser.version ? browser.version : "")
-  url.searchParams.set('deviceModel', device.model ? device.model : "")
-  url.searchParams.set('deviceType', device.type ? device.type : "")
-  url.searchParams.set('osName', os.name ? os.name : "")
-console.log(agent);
+  const url = request.nextUrl.pathname
+  const secretKey = process.env.TOKEN;
+  const agent = request.headers.get("User-Agent")
+  const agentId = request.headers.get("Agent-Id")
+  const deviceId = request.headers.get("Device-Id")
+  const token = request.headers.get("Authorization")
 
-  return NextResponse.rewrite(url)
+
+  if (!agent || !deviceId || !agentId) {
+    return NextResponse.json({ error: "Invalid request sources" }, { status: 404 })
+  }
+  if (url !== "/apis/user/login") {
+    const ts = token?.split(" ")?.[1]
+    if (!ts) {
+      return NextResponse.json({ error: "Empty user token" }, { status: 404 })
+    }
+    try {
+      const decoded = jwt.verify(token, secretKey ? secretKey : "ecbd") as PlayLoad
+      const { username, userId } = decoded;
+      
+
+    } catch (error) {
+      return NextResponse.json({ error: "Invalid user token" }, { status: 404 })
+    }
+  }
+
+
+  return NextResponse.next()
 }
 
 export const config = {
-  matcher: '/apis/:path*',
+  matcher: '/apis/user/:path*',
 }
