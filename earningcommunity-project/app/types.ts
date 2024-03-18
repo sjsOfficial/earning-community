56b1254f4b91cc484f9bf3232770fbc7b427bbb8
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-03-18 11:38:18 +0600

    added video slider

--- earningcommunity-project/app/types.ts
+++ earningcommunity-project/app/types.ts
@@ -4,12 +4,23 @@ amount: number,
 duration: string,
 description: string,
 }
+export interface VideoCardData{
+    videoUrl: string;
+    title: string;
+    date: string;
+    duration: number;
+}
 export interface PackageCardSliderProps{
     packageData: PackageCardData[]
-
+}
+export interface VideoSliderProps{
+    videoCardData: VideoCardData[]
 }
 export interface PackageCardProps{
     packageCardData: PackageCardData
     bgColor: string
 
+}
+export interface VideoCardProps{
+    videoData: VideoCardData
 }
