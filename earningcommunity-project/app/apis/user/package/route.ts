20d5f5ecb0c9fb5766cb0a2bdcf22fb88caf4366
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2024-03-24 16:38:28 +0600

    df

--- earningcommunity-project/app/apis/user/package/route.ts
+++ earningcommunity-project/app/apis/user/package/route.ts
@@ -1,5 +1,5 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const GET=async(request:NextRequest)=>{
-    
+    return NextResponse.json("ok")
 }
