a4d219187d74132061b046ac2e327f2fbe3e64c2
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-04-02 16:29:04 +0600

    added policies

--- earningcommunity-project/app/pages/policies/page.tsx
+++ earningcommunity-project/app/pages/policies/page.tsx
@@ -1,9 +1,137 @@
-import React from 'react'
+import React from "react";
 
 export default function page() {
   return (
-    <div>
-      policies
+    <div className="container mx-auto px-2 pt-4 md:pt-40">
+      <div className="text-white">
+        <ol className="list-decimal mb-8">
+          <li className="mb-4">
+            <strong>Monetization Guidelines:</strong>
+            <ul className="list-disc pl-8">
+              <li>
+                All monetization activities must comply with the platforms
+                policies and terms of service.
+              </li>
+              <li>
+                Creators are encouraged to produce high-quality and engaging
+                content to maximize revenue potential.
+              </li>
+              <li>
+                Any form of fraudulent or deceptive practices, including view
+                botting or fake engagement, will result in immediate termination
+                of the users account.
+              </li>
+            </ul>
+          </li>
+          <li className="mb-8">
+            <strong>Community Engagement Policy:</strong>
+            <ul className="list-disc pl-8">
+              <li>
+                Encourage active participation within the community through
+                collaborations, tournaments, and discussions.
+              </li>
+              <li>
+                Respectful and inclusive behavior is expected from all members.
+                Any form of harassment or discrimination will not be tolerated.
+              </li>
+              <li>
+                Constructive feedback and support among members are essential
+                for fostering a positive and thriving community environment.
+              </li>
+            </ul>
+          </li>
+          <li className="mb-8">
+            <strong>Content Guidelines:</strong>
+            <ul className="list-disc pl-8">
+              <li>
+                Content must adhere to the platforms standards and guidelines,
+                including but not limited to copyright laws and community
+                standards.
+              </li>
+              <li>
+                Graphic or explicit content should be appropriately labeled and
+                restricted to appropriate audiences.
+              </li>
+              <li>
+                Content promoting violence, hate speech, or illegal activities
+                is strictly prohibited.
+              </li>
+            </ul>
+          </li>
+          <li className="mb-8">
+            <strong>Revenue Sharing and Payment Policy:</strong>
+            <ul className="list-disc pl-8">
+              <li>
+                Revenue generated through monetization features will be
+                distributed according to the platforms revenue sharing model.
+              </li>
+              <li>
+                Payments will be processed on a regular basis, subject to
+                minimum payout thresholds and payment processing fees.
+              </li>
+              <li>
+                Creators are responsible for providing accurate payment
+                information and ensuring compliance with tax regulations in
+                their respective jurisdictions.
+              </li>
+            </ul>
+          </li>
+          <li className="mb-8">
+            <strong>Data and Privacy Policy:</strong>
+            <ul className="list-disc pl-8">
+              <li>
+                The platform collects and processes user data in accordance with
+                applicable data protection laws and regulations.
+              </li>
+              <li>
+                User privacy and data security are prioritized, and measures are
+                in place to safeguard personal information.
+              </li>
+              <li>
+                Creators are responsible for respecting the privacy rights of
+                their audience and obtaining necessary consent for data
+                collection and processing.
+              </li>
+            </ul>
+          </li>
+          <li className="mb-8">
+            <strong>Content Ownership and Licensing:</strong>
+            <ul className="list-disc pl-8">
+              <li>
+                Creators retain ownership of their original content uploaded to
+                the platform.
+              </li>
+              <li>
+                By uploading content, creators grant the platform a
+                non-exclusive license to distribute and monetize their content
+                on the platform.
+              </li>
+              <li>
+                Any use of third-party content must be authorized and properly
+                credited according to copyright laws and licensing agreements.
+              </li>
+            </ul>
+          </li>
+          <li>
+            <strong>Enforcement and Compliance:</strong>
+            <ul className="list-disc pl-8">
+              <li>
+                Violations of company policies may result in disciplinary
+                actions, including account suspension or termination.
+              </li>
+              <li>
+                The platform reserves the right to modify or update policies as
+                necessary to ensure compliance with legal requirements and
+                industry standards.
+              </li>
+              <li>
+                Users are encouraged to report any violations or concerns to the
+                platforms support team for prompt investigation and resolution.
+              </li>
+            </ul>
+          </li>
+        </ol>
+      </div>
     </div>
-  )
+  );
 }
