71dff2ea4d67b7ec6cecf9c2d12722c9738446b9
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2024-04-23 10:28:04 +0600

    vbc

--- earningcommunity-project/app/apis/category/route.ts
+++ earningcommunity-project/app/apis/category/route.ts
@@ -7,11 +7,25 @@ interface CATEGORY {
 
 import { NextRequest, NextResponse } from "next/server";
 import category from "./category.json"
+import prisma from "@/libs/prisma";
 
 
 const GET = async (request: NextRequest) => {
-
-    return NextResponse.json(category)
+    const includeVideo = request.nextUrl.searchParams.get("includeVideo")
+    if (!includeVideo) {
+        return NextResponse.json(category)
+    }
+    try {
+        const categories = await Promise.all(category.map(async (cat) => {
+            const count = await prisma.videos.count({
+                where: { categoryId: cat.id }
+            });
+            return { ...cat, count: count };
+        }));
+        return NextResponse.json(categories)
+    } catch (error) {
+        return NextResponse.json({ error: "Error while fetching category", code: error }, { status: 404 })
+    }
 }
- 
+
 export { GET }
