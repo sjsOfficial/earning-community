aba1b8040cafd27c17a6ab7bfb829a57d7cddc42
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-05-07 13:33:17 +0600

    ok

--- earningcommunity-project/app/apis/cookies/route.ts
+++ earningcommunity-project/app/apis/cookies/route.ts
@@ -6,10 +6,10 @@ const GET = async (request: NextRequest) => {
     try {
         const agent = userAgent(request)
         const cookieStore = cookies()
-        cookieStore.set("os", agent.ua, { expires: 120 })
-        cookieStore.set("id", agent.browser.name + "-" + agent.browser.version, { expires: 120 })
+        cookieStore.set("os", agent.ua)
+        cookieStore.set("id", agent.browser.name + "-" + agent.browser.version)
         const res = await axios.get("https://api.ipify.org?format=json");
-        cookieStore.set("ip", res.data.ip, { expires: 120 })
+        cookieStore.set("ip", res.data.ip)
 
         return NextResponse.json({ agent: agent, ip: res.data.ip })
     } catch (error) {
